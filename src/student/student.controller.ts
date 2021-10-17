417c4b2f93f986554dd5a9d947499f8c9864db7c
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-10-17 20:11:24 +0700

    🛠 created student controller

---
+++ src/student/student.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { StudentService } from './student.service';
+
+@Controller('student')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Get()
+  getAllStudents(): string {
+    return this.studentService.getAllStudentsService();
+  }
+}
