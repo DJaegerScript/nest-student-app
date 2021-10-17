c89b77f993708ad6275b6a40f7f7b25b27ea0087
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-10-17 19:18:25 +0700

    🔨 configured database connection

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { StudentModule } from 'src/student/student.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      synchronize: true,
+    }),
+    StudentModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
